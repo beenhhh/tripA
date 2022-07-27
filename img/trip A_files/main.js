cbdccd22197bddd7527f163187cad324a896fafb
beenhhh <97801815+beenhhh@users.noreply.github.com>
2022-07-27 03:59:53 -0700

    Initial commit

---
+++ img/trip A_files/main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+        type:"fraction"
+    },
+
+
+    navigation: {
+        nextEl: '.next_btn',
+        prevEl: '.prev_btn',
+    },
+
+    
+    });
